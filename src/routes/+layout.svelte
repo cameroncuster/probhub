646e665f6fdf1175b2b2872163b13ead5d6de229
Cameron Custer <custer.cameron@gmail.com>
2025-03-08 23:48:41 -0800

    QOL improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Header from '$lib/header/Header.svelte';
-  import Footer from '$lib/footer/Footer.svelte';
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
   import '../app.css';
   import { onMount } from 'svelte';
   import { initAuth } from '$lib/services/auth';
